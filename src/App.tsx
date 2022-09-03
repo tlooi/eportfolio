3481dbb99ebefc1c49eb06585a95fde962d90c29
tlooi <t.looi@outlook.com>
2022-09-03 14:07:19 +1200

    moved navbar into seperate component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
+import Navbar from './Components/Navbar';
 import Header from './Components/Header';
 
 import './App.css'
 
 function App() {
   return (
-    <div className="App">
+    <>
+        <Navbar />
         <Header />
-    </div>
+    </>
   )
 }
 
