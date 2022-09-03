f43d1ab84089d25b455a8742af4eb0749b12219b
tlooi <t.looi@outlook.com>
2022-09-03 12:40:53 +1200

    fixed scrolling with nav open

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -7,6 +7,7 @@ function Header() {
     const menuNav = useRef(null);
     
     const navClasses = 'mob-nav-access ' + (mobNavState ? 'opened' : 'closed');
+    document.body.style.overflowY = mobNavState ? 'hidden' : 'visible';
     
     return (
         <header>
@@ -17,11 +18,11 @@ function Header() {
                         <span></span>
                     </div>
                 </div>
-                <ul className='nav-items'>
-                    <li className='nav-item'><a href="">About Me</a></li>
-                    <li className='nav-item'><a href="">Projects</a></li>
-                    <li className='nav-item'><a href="">Technologies</a></li>
-                    <li className='nav-item'><a href="">Contact Me</a></li>
+                <ul onClick={() => {setMobNavState(false);}} className='nav-items'>
+                    <li className='nav-item'><a href="#">About Me</a></li>
+                    <li className='nav-item'><a href="#">Projects</a></li>
+                    <li className='nav-item'><a href="#">Technologies</a></li>
+                    <li className='nav-item'><a href="#">Contact Me</a></li>
                 </ul>
                 <svg className='animated-logo' width="69" height="35" viewBox="0 0 69 35" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M1 11.6412L43.9588 1C36.9961 2.57647 22.8341 6.91176 21.8882 11.6412C20.7059 17.5529 -4.12353 51.0529 30.9529 22.6765C59.0141 -0.0247053 37.6529 15.0569 30.9529 19.1294C19.1294 26.3163 29.7706 32.1353 35.6824 27.4059C36.9961 27.0118 40.3329 25.9082 47.9 15.1882C55.4671 4.46824 57.4902 13.0863 55.7824 19.1294C60.249 12.9549 68.6306 6.36 63.2706 15.1882C56.5706 26.2235 61.6941 32.1353 68 27.4059" stroke="black" strokeWidth="2"/>
