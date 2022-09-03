b7aba74f7523384440d7721d1352ac9190aaee7f
tlooi <t.looi@outlook.com>
2022-09-03 14:31:53 +1200

    make header of page

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -3,7 +3,13 @@ import './Header.css';
 function Header() {
     return (
         <header>
-            
+            <div className='container'>
+                <div className='header-text-wrapper'>
+                    <p className='header-welcome-text'>👋Hey I'm Timothy!</p>
+                    <p className='header-self-description'>A motivated Software Engineer currently studying at the University of Auckland. I am currently interested in frontend development with hopes to become a fullstack. I also like to play around with the huge ecosystem surrounding software include Neural Networks and Games. Asides from coding, I am an avid piano player, and love challeneges like solving multiple variations of Rubik's cubes, skiing, photography, and playing within the University of Auckland's UNIM8S Futsal Social League.</p>
+                </div>
+                <a href="#" className='contact-me-button'>Contact Me</a>
+            </div>
         </header>
     );
 }
